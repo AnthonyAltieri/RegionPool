54182c3b17e68d5ab83d543c400c5775ae246daf
anthonyaltieri <Anthonyraltieri@gmail.com>
2016-10-26 15:21:21 -0700

    added a button to get to dash destination

---
+++ Frontend/src/components/Dash/Destination.js
@@ -0,0 +1,12 @@
+/**
+ * @author Anthony Altieri on 10/26/16.
+ */
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Destination extends Component {
+
+}
+
+export default Destination;
--- Frontend/src/components/Dash/Main.js
+++ Frontend/src/components/Dash/Main.js
@@ -4,17 +4,33 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import FontIcon from 'material-ui/FontIcon';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 
 class Main extends Component {
   render() {
+    const { goToDashDestination } = this.props;
+
     return (
       <div className="main fullscreen">
         <div className="container-map">
           <img src={require('../../../img/map.png')} />
         </div>
+        <FloatingActionButton
+          style={{
+            position: "absolute",
+            bottom: "64px",
+            right: "12px",
+          }}
+          onClick={() => {
+            goToDashDestination();
+          }}
+        >
+          <FontIcon className="material-icons">directions_bus</FontIcon>
+        </FloatingActionButton>
         <div className="box-info">
           <p>0.2 miles from nearest pick-up region</p>
-
         </div>
 
       </div>
@@ -26,6 +42,9 @@ const stateToProps = (state) => ({
 });
 
 const dispatchToProps = (dispatch) => ({
+  goToDashDestination: () => {
+    dispatch(push('/dash/destination'));
+  }
 });
 
 Main = connect(
